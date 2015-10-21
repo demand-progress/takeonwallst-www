62d2f63e3d267639b0c37d1cd822c0545371fe40
Chris Antaki <ChrisAntaki@gmail.com>
2015-10-21 00:59:39 -0700

    Resetting hash after landing on a modal triggering hashed URL

--- js/sunset.js
+++ js/sunset.js
@@ -208,9 +208,11 @@ if (window.location.href.indexOf('call=1') != -1) {
 if (location.hash === '#confirmed') {
     modal_show('confirmed');
     showThanks();
+    location.hash = '';
 } else if (location.hash === '#sent') {
     modal_show('sent');
     showThanks();
+    location.hash = '';
 }
 
 function showThanks() {
