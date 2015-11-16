fa950b95594ed8fdcab06d19848f6ee0256ddb9c
Chris Antaki <ChrisAntaki@gmail.com>
2015-11-16 12:40:34 -0800

    Previewable more info section

--- js/bundle.js
+++ js/bundle.js
@@ -207,6 +207,10 @@ $(function () {
         fetchCallCount();
     }
 
+    if (StaticKit.query.previewMoreInfo) {
+        $('body').addClass('preview-more-info');
+    }
+
     function numberWithCommas(x) {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
